655273b4b0834d7fe535bf23aeecf9b9dad6162d
hbuminy <pybhad@gmail.com>
2023-06-01 17:52:59 +0300

    added my own cv project as an example

--- mytasarim/src/lib/mycv.svelte
+++ mytasarim/src/lib/mycv.svelte
@@ -1,33 +1,140 @@
-<div class="w-full h-full bg-white flex flex-col p-5 gap-5">
-    <div class="flex flex-row w-full bg-slate-50 shadow-md p-2 justify-between">
-        <div class="flex flex-col ">
-            <h1 class="text-4xl bebas">
-                Hulusi Bumin Yalçın
-            </h1>
-            <h1 class="text-2xl bebas">
-                Entry-level Web-developer
-            </h1>
-            <p class="text-sm">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum repellendus nemo ullam molestias, sit necessitatibus quod! Sit, facere? Consectetur eveniet, quis odit tempora illo blanditiis est vero dolorem veniam nisi!
-            </p>
+<script>
+    import IoIosMail from 'svelte-icons/io/IoIosMail.svelte'
+    import FaPhone from 'svelte-icons/fa/FaPhone.svelte'
+    import FaLink from 'svelte-icons/fa/FaLink.svelte'
+    import FaGithub from 'svelte-icons/fa/FaGithub.svelte'
+</script>
+
+<div class="relative w-full h-full bg-white flex flex-col p-5 gap-5 z-0 overflow-hidden">
+
+    <div class="z-0 relative">
+        <div class="absolute top-0 left-0 w-full h-full translate-x-1 translate-y-1 bg-slate-300 z-10"></div>
+
+        <div class="relative flex flex-col w-full bg-slate-50 p-2 z-20">
+
+            <div class="flex flex-row justify-between">
+                
+                <div class="flex flex-col text-slate-700">
+                    <h1 class="text-4xl bebas">
+                        Hulusi Bumin Yalçın
+                    </h1>
+                    <h1 class="text-2xl bebas">
+                        Entry-level <span class="text-red-600">Web</span>-developer
+                    </h1>
+                </div>
+
+                <div class="flex flex-col w-44 text-sm text-slate-500">
+
+                    <div class="flex flex-row items-center">
+                        <div class="listBullet">pybhad@gmail.com</div>
+                        
+                    </div>
+
+                    <div class="flex flex-row items-center">
+                        <div class="listBullet">551 043 99 79</div>
+                        
+                    </div> 
+
+                    <div class="flex flex-row items-center">
+                        <div class="listBullet"><a href="https://buminyalc.in/">buminyalc.in</a></div>
+                    </div> 
+
+                    <div class="flex flex-row items-center">
+                        <div class="listBullet">github.com/HBuminY</div>
+                        
+                    </div> 
+                </div>
+            </div>
+
+            <div class="border-t-2 border-blue-950 mt-2 pt-2">
+                <p class="text-sm text-slate-700">
+                    Excited web developer equipped with a solid foundation in modern web development tools like Svelte, Tailwind, and Vite. Seeking an opportunity to apply my technical skills and passion for crafting exceptional user experiences in a dynamic and collaborative environment.
+                    I value continous learning, innovation, creativity, and excellence.
+                    With a strong problem-solving aptitude and a keen eye for clean and efficient code, I aim to make a positive impact building great web applications.
+                </p>
+            </div>
         </div>
-        <div>
-            <ul class="list-none text-right text-xs">
-                <li>pybhad@gmail.com</li>
-                <li>551 043 99 79</li>
-                <li>buminyalc.in</li>
-                <li>github.com/HBuminY</li>
-            </ul>
-        </div>
+        
     </div>
 
+    <div class="z-0 relative grow">
+        <div class="absolute top-0 left-0 w-full h-full translate-x-1 translate-y-1 bg-slate-300 z-10"></div>
+        <div class="relative flex flex-col justify-between grow bg-slate-50 z-20 h-full p-1">
 
+            <div class="bg-slate-200 p-2 rounded-md">
+            <h1 class="text-red-600 bebas text-xl">Work Experience :</h1>
 
-    <div class="flex flex-col grow bg-slate-50 shadow-md">
-        <div>
-            <h1 class="">
-                Work Experience
-            </h1>
+            <div class="flex flex-row">
+                <div class="shrink-0">
+                    <h1 class="bebas text-lg text-slate-600 listBullet">Freelence Developer</h1>
+                    <h1 class="bebas text-sm text-slate-400 -translate-y-2 text-center">2022 - present</h1>
+                </div>
+                <div class="grow flex flex-col">
+                    <div class="ml-10 text-xs text-slate-500">
+                        <div class="listBullet">As a freelance developer, I successfully completed multiple projects for diverse clients.</div>
+                        <div class="listBullet">Collaborated with clients to understand their requirements and delivered tailored solutions that met their objectives.</div>
+                        <div class="listBullet">Rapidly adapted to changing project requirements.</div>
+                    </div>
+                </div>
+                <div class="ml-auto">
+                    <h1 class="bebas text-sm text-slate-400 float-right">Antalya</h1>
+                </div>
+            </div>
+            </div>
+
+            <h1 class="text-red-600 bebas text-xl">Projects :</h1>
+
+            <div>
+                <div class="flex flex-row">
+                    <div class="bebas">
+                        <h1 class="text-lg text-slate-600 listBullet">Wanderstone <span class="bebas text-sm text-slate-400 align-middle">- Founder</span></h1>
+                        <h1 class="text-sm text-slate-400 -translate-y-2 text-center">2021</h1>
+                    </div>
+                    <div class="grow">
+                        <h1 class="bebas text-sm text-slate-400 float-right">İstanbul</h1>
+                    </div>
+                </div>
+                <div><ul class="list-none list-inside text-sm text-slate-600 ml-5 -translate-y-3">
+                    <li class="listBullet">Developed a full-on 2D desktop game with GMS2 game engine.</li>
+                    <li class="listBullet">Developed a web application that works integrated with the video game.</li>
+                    <li class="listBullet">Built a custom server for the web app. using Node.js and Express.js .</li>
+                    <li class="listBullet">Designed and codded a custom front-end for the web app. using Bootstrap and SCSS.</li>
+                    <li class="listBullet">Lead the developer team and got a finalist degree in Teknofest-2021 with this project.</li>
+                </ul></div>
+            </div>
+            
+            <div class="border-t-2 border-slate-400 mt-2 pt-2">
+                <div class="flex flex-row">
+                    <div class="bebas">
+                        <h1 class="text-lg text-slate-600 listBullet">MyTasarim <span class="bebas text-sm text-slate-400 align-middle">- Creator</span></h1>
+                        <h1 class="text-sm text-slate-400 -translate-y-2 text-center">2023</h1>
+                    </div>
+                    <div class="grow">
+                        <h1 class="bebas text-sm text-slate-400 float-right">///</h1>
+                    </div>
+                </div>
+                <div><ul class="list-none list-inside text-sm text-slate-600 ml-5 -translate-y-3">
+                    <li class="listBullet">Development of the very tool that made this vc's design possible.</li>
+                    <li class="listBullet">A Vite/Svelte based project for building and saving PDFs with your favorite front-end tools.</li>
+                    <li class="listBullet">Made the process of designing documents much easier with still 100% creative control.</li>
+                    <li class="listBullet">Developed and published fully open-source on github.</li>
+                </ul></div>
+            </div>
+
+            <h1 class="text-red-600 bebas text-xl">Skills :</h1>
+            <p class="text-sm text-slate-600">
+                <span class="listBullet mx-1">HTML/CSS/SCSS</span>
+                <span class="listBullet mx-1">Javascript</span>
+                <span class="listBullet mx-1">Node.js</span>
+                <span class="listBullet mx-1">Svelte.js</span>
+                <span class="listBullet mx-1">TailwindCSS</span>
+                <br>
+                <span class="listBullet mx-1">Verbal and Written Fluent English</span>
+                <span class="listBullet mx-1">Native Speaker in Turkish</span>
+                <span class="listBullet mx-1">Building Documentation</span>
+                <span class="listBullet mx-1">Report Writing in Academic Standarts</span>
+                <span class="listBullet mx-1">Presentation</span>
+            </p>
         </div>
     </div>
 </div>
@@ -40,6 +147,5 @@
 
     .bebas{
         font-family: 'bebas', sans-serif;
-        @apply text-slate-900;
     }
 </style>
